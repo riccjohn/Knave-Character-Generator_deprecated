f0817b507d95a35478dd8d35fa7be9270f566deb
riccjohn <riccjohn@gmail.com>
2018-11-27 14:11:16 -0600

    Updates

---
+++ precache-manifest.c3088fc1c69d22640f177bff108fe718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "537dffda38f147927637",
+    "url": "/Knave-Character-Generator/static/js/runtime~main.537dffda.js"
+  },
+  {
+    "revision": "75e2ac6ae32233a2743b",
+    "url": "/Knave-Character-Generator/static/js/main.75e2ac6a.chunk.js"
+  },
+  {
+    "revision": "79f292c14da9687a40dd",
+    "url": "/Knave-Character-Generator/static/js/1.79f292c1.chunk.js"
+  },
+  {
+    "revision": "75e2ac6ae32233a2743b",
+    "url": "/Knave-Character-Generator/static/css/main.f9ff8e43.chunk.css"
+  },
+  {
+    "revision": "22c7228b0a9a0c8cd9a8689744b45298",
+    "url": "/Knave-Character-Generator/index.html"
+  }
+];
