d60e8029dba0f79b5722e6db05acdc7e2347d609
John Riccardi <riccjohn@gmail.com>
2018-11-28 11:51:00 -0600

    Updates

--- precache-manifest.eabab3feda477a927b8e267778bd6750.js
+++ precache-manifest.eabab3feda477a927b8e267778bd6750.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Knave-Character-Generator/static/js/runtime~main.537dffda.js"
   },
   {
-    "revision": "9e1ccd6526d8eb9b9e15",
-    "url": "/Knave-Character-Generator/static/js/main.9e1ccd65.chunk.js"
+    "revision": "e931dbc3264567a9f802",
+    "url": "/Knave-Character-Generator/static/js/main.e931dbc3.chunk.js"
   },
   {
     "revision": "79f292c14da9687a40dd",
     "url": "/Knave-Character-Generator/static/js/1.79f292c1.chunk.js"
   },
   {
-    "revision": "9e1ccd6526d8eb9b9e15",
-    "url": "/Knave-Character-Generator/static/css/main.ddd6b988.chunk.css"
+    "revision": "e931dbc3264567a9f802",
+    "url": "/Knave-Character-Generator/static/css/main.77ccf939.chunk.css"
   },
   {
-    "revision": "cfa4b09c10ea430230289c148a13f7ca",
+    "revision": "f69048d2905a31a4a6974b0914a1c33f",
     "url": "/Knave-Character-Generator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Knave-Character-Generator/precache-manifest.064cf1c7d57aa281df96749401416184.js"
+  "/Knave-Character-Generator/precache-manifest.eabab3feda477a927b8e267778bd6750.js"
 );
 
 workbox.clientsClaim();
