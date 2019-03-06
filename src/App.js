508191835baedf04d3767327ee90efbbca2966df
riccjohn <riccjohn@gmail.com>
2019-03-06 13:03:47 -0600

    chore: Fix eslint rules to work with React imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+import React, { Component } from 'react';
 import { AbilityScores, CharacterDetails, Traits } from './Components';
 import './index.css';
 import { weapons } from './data/gear';
@@ -22,9 +22,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="main-container flex column">
-        <div className="main-padding flex column">
-          <h1 className="box">Knave</h1>
+      <div className='main-container flex column'>
+        <div className='main-padding flex column'>
+          <h1 className='box'>Knave</h1>
           <Traits character={this.state} />
           <AbilityScores character={this.state} />
           <CharacterDetails
@@ -33,17 +33,17 @@ class App extends Component {
           />
         </div>
 
-        <div className="footer flex row">
-          <p className="block">
+        <div className='footer flex row'>
+          <p className='block'>
             Based on the fantasic RPG{' '}
-            <a href="https://www.drivethrurpg.com/product/250888/Knave">
-              'Knave'
+            <a href='https://www.drivethrurpg.com/product/250888/Knave'>
+              {'Knave'}
             </a>
             {' by '}
-            <a href="https://www.youtube.com/watch?v=hVL4coIiHsE">Ben Milton</a>
+            <a href='https://www.youtube.com/watch?v=hVL4coIiHsE'>Ben Milton</a>
           </p>
-          <p className="block">
-            Made with <span id="heart">♥</span> in Madison, WI
+          <p className='block'>
+            Made with <span id='heart'>♥</span> in Madison, WI
           </p>
         </div>
       </div>
